640dd2d1c7e5bd10d42d4a3bd1595962c976d1b7
Nestor Rubio <nestordrubio9@gmail.com>
2024-05-02 20:11:07 -0400

    added a description endpoint

--- routes/v1.go
+++ routes/v1.go
@@ -10,4 +10,7 @@ func V1(g *gin.RouterGroup) {
 	g.GET("/healthcheck", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "ok"})
 	})
+	g.GET("/description", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Nestor Rubio's personal API :) <3"})
+	})
 }
